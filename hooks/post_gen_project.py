c392cb553d79d557310355b78dd130d6f2dec9d6
Brandon <tricksterguy87@gmail.com>
2025-10-28 01:05:00 -0700

    new minimal clan example and template options to enable/disable it.

---
+++ hooks/post_gen_project.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+import os
+import shutil
+import yaml
+
+MANIFEST = "manifest.yml"
+
+
+def delete_resources_for_disabled_features():
+    with open(MANIFEST) as manifest_file:
+        manifest = yaml.load(manifest_file)
+        for feature in manifest['features']:
+            if (feature['enabled'] == 'no' and not feature['inverted']) or 
+               (feature['enabled'] == 'yes' and feature['inverted']):
+                
+                print "removing resources for {} feature {}...".format('enabled' if feature['inverted'] else 'disabled', feature['name'])
+                for resource in feature['resources']:
+                    delete_resource(resource)
+    print "cleanup complete, removing manifest..."
+    delete_resource(MANIFEST)
+
+
+def delete_resource(resource):
+    if os.path.isfile(resource):
+        print "removing file: {}".format(resource)
+        os.remove(resource)
+    elif os.path.isdir(resource):
+        print "removing directory: {}".format(resource)
+        shutil.rmtree(resource)
+
+if __name__ == "__main__":
+    delete_resources_for_disabled_features()
