01f4505e678e6d26b89b387ae63973b2004596e1
Brandon <tricksterguy87@gmail.com>
2025-10-28 01:50:24 -0700

    fixes

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -8,26 +8,35 @@ from pathlib import Path
 def delete_resources_for_disabled_features():
     script_dir = Path(__file__).resolve().parent
     manifest_path = script_dir / "manifest.yml"
-    with open(manifest_path) as manifest_file:
-        manifest = yaml.load(manifest_file)
-        for feature in manifest['features']:
-            if ((feature['enabled'] == 'no' and not feature['inverted']) or 
-               (feature['enabled'] == 'yes' and feature['inverted'])):
-                
-                print("removing resources for {} feature {}...".format('enabled' if feature['inverted'] else 'disabled', feature['name']))
-                for resource in feature['resources']:
-                    delete_resource(resource)
+
+    # The root of the generated project
+    project_root = Path.cwd()
+
+    # Load the manifest
+    with open(manifest_path, encoding="utf-8") as manifest_file:
+        manifest = yaml.safe_load(manifest_file)
+
+    for feature in manifest['features']:
+        if ((feature['enabled'] == 'no' and not feature['inverted']) or
+           (feature['enabled'] == 'yes' and feature['inverted'])):
+
+            print("removing resources for {} feature {}...".format(
+                'enabled' if feature['inverted'] else 'disabled', feature['name']))
+            for resource in feature['resources']:
+                delete_resource(project_root / resource)
+
     print("cleanup complete, removing manifest...")
-    delete_resource(MANIFEST)
+    delete_resource(manifest_path)
 
 
 def delete_resource(resource):
-    if os.path.isfile(resource):
+    if resource.is_file():
         print("removing file: {}".format(resource))
-        os.remove(resource)
-    elif os.path.isdir(resource):
+        resource.unlink()
+    elif resource.is_dir():
         print("removing directory: {}".format(resource))
         shutil.rmtree(resource)
+
 
 if __name__ == "__main__":
     delete_resources_for_disabled_features()
