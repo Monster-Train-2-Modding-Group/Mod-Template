30910f81c379bb24c16515f4d9ec227d7af4379f
Brandon <tricksterguy87@gmail.com>
2025-10-28 01:32:45 -0700

    fix python2 syntax error on print

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -13,19 +13,19 @@ def delete_resources_for_disabled_features():
             if ((feature['enabled'] == 'no' and not feature['inverted']) or 
                (feature['enabled'] == 'yes' and feature['inverted'])):
                 
-                print "removing resources for {} feature {}...".format('enabled' if feature['inverted'] else 'disabled', feature['name'])
+                print("removing resources for {} feature {}...".format('enabled' if feature['inverted'] else 'disabled', feature['name']))
                 for resource in feature['resources']:
                     delete_resource(resource)
-    print "cleanup complete, removing manifest..."
+    print("cleanup complete, removing manifest...")
     delete_resource(MANIFEST)
 
 
 def delete_resource(resource):
     if os.path.isfile(resource):
-        print "removing file: {}".format(resource)
+        print("removing file: {}".format(resource))
         os.remove(resource)
     elif os.path.isdir(resource):
-        print "removing directory: {}".format(resource)
+        print("removing directory: {}".format(resource))
         shutil.rmtree(resource)
 
 if __name__ == "__main__":
